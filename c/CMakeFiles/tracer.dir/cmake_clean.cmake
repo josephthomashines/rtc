2bb402ceeb82d75be2fa5f0d77b8ab9f00b9bc90
Joseph Hines <jthines3@gmail.com>
2020-04-07 19:09:33 -0400

    Started c implementation 😁

---
+++ c/CMakeFiles/tracer.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tracer.dir/main.c.o"
+  "CMakeFiles/tracer.dir/src/primitives.c.o"
+  "CMakeFiles/tracer.dir/src/say_hello.c.o"
+  "out/tracer"
+  "out/tracer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/tracer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ c/include/primitives.h
@@ -0,0 +1,13 @@
+#ifndef __PRIMITIVES_H__
+#define __PRIMITIVES_H__
+
+// Base primtive type
+typedef struct Primitive Primitive;
+
+// Create the two kinds of primitives
+*Primitive new_point(float x, y, z);
+*Primitive new_vector(float x, y, z);
+
+
+
+#endif
