2f9891852d41d155c1ceadfbf1741cec3b7b3050
Joseph Hines <jthines3@gmail.com>
2020-04-08 16:15:03 -0400

    .

---
+++ c/test/test_tracer.c
@@ -0,0 +1,17 @@
+#define CHEAT_NO_MATH
+
+#include "cheat.h"
+#include "cheats.h"
+
+// ---------------------------------------------------------------------------
+// Test Primitives
+
+CHEAT_TEST(empty,
+        cheat_assert(1==1);
+)
+
+CHEAT_TEST(empty2,
+        cheat_assert(2==2);
+)
+
+// ---------------------------------------------------------------------------
