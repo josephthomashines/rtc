93445e2e6de19a56969171ad3363062da49c7698
Joseph Hines <jthines3@gmail.com>
2020-05-11 22:26:29 -0400

    .

--- py/main.py
+++ py/main.py
@@ -16,8 +16,19 @@ class Primitive():
     def __str__(self):
         return f'{self._x} {self._y} {self._z} {self._w} '
 
+    def is_point(self):
+        return self._w == POINT
+    def is_vector(self):
+        return self._w == VECTOR
+    def is_valid(self):
+        return (not self.is_point()) and (not self.is_vector())
+
+class Point(Primitive):
+    def __init__(self,x=0,y=0,z=0):
+        super().__init__(POINT,x,y,z)
+
 def main():
-    p = Primitive(POINT)
+    p = Point()
     print(p)
 
 if __name__ == "__main__":
