70baea54bee8a46576289eb9889e27a7cf51e174
Joseph Hines <jthines3@gmail.com>
2020-06-23 14:07:35 -0400

    .

--- objects.go
+++ objects.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"math"
+	"sort"
 )
 
 type ray struct {
@@ -30,29 +31,79 @@ func (r *ray) Position(t float64) *primitive {
 	return p
 }
 
-func (r *ray) IntersectSphere(s *sphere) []float64 {
-	sphereToRay := r.origin.Sub(NewPoint(0,0,0))
-	a := r.direction.Dot(r.direction)
-	b := 2 * r.direction.Dot(sphereToRay)
+func (r *ray) IntersectSphere(s *sphere) []*intersection {
+	r2 := r.Transform(s.transform.Inverse())
+
+	sphereToRay := r2.origin.Sub(NewPoint(0,0,0))
+	a := r2.direction.Dot(r2.direction)
+	b := 2 * r2.direction.Dot(sphereToRay)
 	c := sphereToRay.Dot(sphereToRay) - 1
 	disc := math.Pow(b,2) - 4 * a * c
 
 	if disc < 0 {
-		return []float64{}
+		return []*intersection{}
 	}
 
 	t1 := (-b - math.Sqrt(disc)) / (2*a)
 	t2 := (-b + math.Sqrt(disc)) / (2*a)
 
-	return []float64{t1,t2}
+	return Intersections(NewIntersection(t1,s),NewIntersection(t2,s))
+}
+
+func (r *ray) Transform(tr *matrix) *ray {
+	return NewRay(
+		tr.MultiplyPrimitive(r.origin),
+		tr.MultiplyPrimitive(r.direction),
+	)
+}
+
+type object interface {
+	GetId() string
 }
 
 type sphere struct {
 	id string
+	transform *matrix
 }
 
 func NewSphere() *sphere {
 	return &sphere{
 		id: UUID(),
+		transform: NewIdentityMatrix(4),
 	}
 }
+
+func (s *sphere) GetId() string {
+	return s.id
+}
+
+type intersection struct {
+	t float64
+	obj object
+}
+
+func NewIntersection(t float64, obj object) *intersection {
+	return &intersection{
+		t: t,
+		obj: obj,
+	}
+}
+
+func Intersections(is ...*intersection) []*intersection {
+	sort.Slice(is, func(i, j int) bool {
+		return is[i].t < is[j].t
+	})
+	return is
+}
+
+func Hit(xs []*intersection) *intersection {
+	for _, x := range xs {
+		if x.t >= 0 {
+			return x
+		}
+	}
+
+	return nil
+}
+
+
--- objects_test.go
+++ objects_test.go
@@ -59,8 +59,126 @@ func TestRayIntersect(t *testing.T) {
 		if len(xs) != counts[i] {
 			t.Error("xs count incorrect")
 		}
-		if counts[i] != 0 && (!FloatEquals(xs[0],xss[i][0]) || !FloatEquals(xs[1],xss[i][1])) {
+		if counts[i] != 0 &&
+		(!FloatEquals(xs[0].t,xss[i][0]) ||
+		!FloatEquals(xs[1].t,xss[i][1]) ||
+		xs[0].obj != s || xs[1].obj != s) {
 			t.Error("xs values incorrect")
 		}
+	}
+
+	r := NewRay(NewPoint(0,0,-5),NewVector(0,0,1))
+	s.transform = NewScaling(2,2,2)
+	xs := r.IntersectSphere(s)
+
+	if len(xs) != 2 ||
+	   !FloatEquals(xs[0].t,3) ||
+	   !FloatEquals(xs[1].t,7) {
+			 t.Error("Ray sphere incorrect when sphere scaled")
+	}
+
+	s.transform = NewTranslation(5,0,0)
+	xs = r.IntersectSphere(s)
+
+	if len(xs) != 0 {
+		t.Error("Ray sphere incorrect when sphere translated")
+	}
+}
+
+func TestNewIntersection(t *testing.T) {
+	s := NewSphere()
+	i := NewIntersection(3.5,s)
+
+	if i.t != 3.5 || i.obj != s {
+		t.Error("Cannot create NewIntersection correctly")
+	}
+}
+
+func TestIntersections(t *testing.T) {
+	s := NewSphere()
+	i1 := NewIntersection(1,s)
+	i2 := NewIntersection(2,s)
+	xs := Intersections(i1,i2)
+
+	if len(xs) != 2 || xs[0].t != 1 || xs[1].t != 2 {
+		t.Error("Cannot properly aggregate intersections")
+	}
+}
+
+func TestHit(t *testing.T) {
+	s := NewSphere()
+
+	i1 := NewIntersection(1,s)
+	i2 := NewIntersection(2,s)
+	xs := Intersections(i1, i2)
+	i := Hit(xs)
+
+	if i != i1 {
+		t.Error("Hit incorrect")
+	}
+
+	i1 = NewIntersection(-1,s)
+	i2 = NewIntersection(1,s)
+	xs = Intersections(i1, i2)
+	i = Hit(xs)
+
+	if i != i2 {
+		t.Error("Hit incorrect")
+	}
+
+	i1 = NewIntersection(-2,s)
+	i2 = NewIntersection(-1,s)
+	xs = Intersections(i1, i2)
+	i = Hit(xs)
+
+	if i != nil {
+		t.Error("Hit incorrect")
+	}
+
+	i1 = NewIntersection(5,s)
+	i2 = NewIntersection(7,s)
+	i3 := NewIntersection(-3,s)
+	i4 := NewIntersection(2,s)
+	xs = Intersections(i1, i2, i3, i4)
+	i = Hit(xs)
+
+	if i != i4 {
+		t.Error("Hit incorrect")
+	}
+}
+
+func TestRayTransform(t *testing.T) {
+	r := NewRay(NewPoint(1,2,3),NewVector(0,1,0))
+	m := NewTranslation(3,4,5)
+	tr := r.Transform(m)
+	exp := NewRay(NewPoint(4,6,8),NewVector(0,1,0))
+
+	if !exp.origin.Equals(tr.origin) ||
+	!exp.direction.Equals(tr.direction) {
+		t.Error("Cannot properly ray transform")
+	}
+
+	m = NewScaling(2,3,4)
+	tr = r.Transform(m)
+	exp = NewRay(NewPoint(2,6,12),NewVector(0,3,0))
+
+	if !exp.origin.Equals(tr.origin) ||
+	!exp.direction.Equals(tr.direction) {
+		t.Error("Cannot properly ray transform")
+	}
+}
+
+func TestSphere(t *testing.T) {
+	s := NewSphere()
+
+	if !s.transform.Equals(NewIdentityMatrix(4)) {
+		t.Error("Sphere transform not initialized correctly")
+	}
+
+	tr := NewTranslation(2,3,4)
+	s.transform = tr
+
+	if !s.transform.Equals(tr) {
+		t.Error("Cannot properly set sphere transform")
 	}
 }
