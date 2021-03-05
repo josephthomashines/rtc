52f5c5e1715d5b0dcf11c8ef64a70316cdb5359e
Joseph Hines <jthines3@gmail.com>
2021-03-05 15:46:10 -0500

    Moved executables to bin, using argp for parsing

--- c/main.c
+++ c/main.c
@@ -1,82 +1,109 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
+#include <argp.h>
 
-#include "src/utils.h"
 
 #include "src/tuples.h"
+#include "src/utils.h"
 
-#define MAX_FULL 16
 
-typedef struct opt_t {
-	char flag[2];
-	char full[MAX_FULL];
-	void (*handle)(int, int, char*[]);
-} opt;
+const char *program_version = "rtc 0.0.1";
+const char *bug_address = "";
 
-void handle_demo(int i, int argc, char* argv[])
-{
+static char doc[] =
+	"  ephjos's C implementation of the Raytracer Challenge";
 
-	typedef struct demo_t {
-		char* name;
-		void (*run)();
-	} demo;
+static char args_doc[] =
+	"";
 
-#define NUM_DEMOS 1
-	demo demos[NUM_DEMOS] = {
-		{ "tuple", demo_tuples }
-	};
-
-	int error = 1;
-
-	if (i+1 < argc) {
-		error = 0;
-		int j = 0;
-		for (; j < NUM_DEMOS; ++j) {
-			if (strncmp(demos[j].name, argv[i+1], 32) == 0) {
-				demos[j].run();
-				return;
-			}
-		}
-		error = 1;
-	}
-
-	if (error) {
-		fprintf(stderr, "Unknown demo name, available demos:\n");
-		int j = 0;
-		for (; j < NUM_DEMOS; ++j) {
-			fprintf(stderr, "\t%s", demos[j].name);
-		}
-		fprintf(stderr, "\n");
-		exit(1);
-	}
-}
-
-#define NUM_OPTS 1
-opt opts[NUM_OPTS] = {
-	{ "-d", "--demo", handle_demo },
+static struct argp_option options[] = {
+	{ "demo", 'd', "DEMO", 0, "Run the demo named DEMO" },
+	{ 0 },
 };
 
-char match_opt(opt o, char* s)
+struct arguments
 {
-	return strncmp(o.flag, s, 2) || strncmp(o.full, s, MAX_FULL);
+	bool no_opts;
+	char *demo;
+};
+
+// Used by parser, call on each opt
+static error_t
+parse_opt(int key, char *arg, struct argp_state *state)
+{
+	struct arguments *arguments = state->input;
+
+	switch(key)
+	{
+		case 'd':
+			arguments->no_opts = false;
+			arguments->demo = arg;
+			break;
+		case ARGP_KEY_ARG:
+			// ignore all args, only use options
+			fprintf(stderr, "ignoring argument: %s\n", arg);
+			break;
+		case ARGP_KEY_END:
+			// If we parse all input and we found no options, print help info and exit
+			if (arguments->no_opts == true) {
+				fprintf(stderr, "Must provide at least one option...\n\n");
+				argp_usage(state);
+			}
+			break;
+		default:
+			return ARGP_ERR_UNKNOWN;
+	}
+	return 0;
 }
 
-int main(int argc, char *argv[])
+// Initialize parser
+static struct argp argp = { options, parse_opt, args_doc, doc };
+
+// Simple abstraction to allow easily adding demos
+struct demo
 {
-	int i = 1;
-	for (; i < argc; ++i) {
-		if (argv[i][0] == '-') {
-			int j = 0;
-			for (; j < NUM_OPTS; ++j) {
-				if (match_opt(opts[j], argv[i])) {
-					opts[j].handle(i, argc, argv);
-					return 0;
-				}
+	char *name;
+	void (*run)();
+};
+
+static struct demo demos[] = {
+	{ "tuples", demo_tuples },
+};
+
+int
+main(int argc, char **argv)
+{
+	// Initialize arguments
+	struct arguments arguments;
+	arguments.no_opts = true;
+	arguments.demo = NULL;
+
+	// Run arg parser
+	argp_parse(&argp, argc, argv, 0, 0, &arguments);
+
+	// Handle running a demo
+	if (arguments.demo != NULL) {
+		long num_demos = sizeof(demos) / sizeof(struct demo);
+		long i = 0;
+		for (; i < num_demos; ++i) {
+			if (strncmp(arguments.demo, demos[i].name, 32) == 0) {
+				demos[i].run();
+				exit(0);
 			}
 		}
+
+		// If argument.demo does not match any known demos, print out all
+		// available demos and exit
+		i = 0;
+		fprintf(stderr, "Unknown demo: %s\n  Available demos: [", arguments.demo);
+		for (; i < num_demos; ++i) {
+			fprintf(stderr, " %s", demos[i].name);
+		}
+		fprintf(stderr, " ]\n");
+		exit(1);
 	}
 
-	printf("Nothing to do...\n");
-	return 0;
+	exit(0);
 }
